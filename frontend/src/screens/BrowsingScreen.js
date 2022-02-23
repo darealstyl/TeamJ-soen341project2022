5d93ab43a70e50d373351f86c38f341bf5a324c5
jaskiratkaur1906 <97757963+jaskiratkaur1906@users.noreply.github.com>
2022-02-23 17:30:25 -0500

    Deletion of code

--- frontend/src/screens/BrowsingScreen.js
+++ frontend/src/screens/BrowsingScreen.js
@@ -45,13 +45,7 @@ function BrowsingScreen() {
               </Col>
           ))}
 
-          {products.slice(1, 5).map(product => (
-              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-
-                <Product product={product} />
-
-              </Col>
-          ))}
+       
 
       </Row>
   </div>
