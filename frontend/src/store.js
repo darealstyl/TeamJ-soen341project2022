108f5d20067e7ef2f1f30c15aeaefe514b54257b
Wassim <nijaouiwassim1@yahoo.com>
2022-03-20 20:22:44 -0400

    sign in changes and fix with working backend
    sign in changes and fix with working backend
    register needs to be fixed next

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,13 +2,28 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools }  from 'redux-devtools-extension';
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
+import { userLoginReducer, userRegisterReducer } from './reducers/userReducers';
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
+     userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
 });
 
-const initialState = {};
+const cartItemsFromStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
+
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
+const initialState = {
+ 
+  userLogin: { userInfo: userInfoFromStorage },
+};
+
 
 const middleware = [thunk];
 
