a4dbabd085b9881567c5c2bf57092c1b66823d0f
z-bruce <87652520+z-bruce@users.noreply.github.com>
2022-04-12 11:18:27 -0400

    Update ProductListScreen.js
    changed isAdmin to isSeller for pagination

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -141,7 +141,7 @@ function ProductListScreen({ history, match }) {
                                 )}
                         </tbody>
                     </Table>
-                    <Paginate pages={pages} page={page} isAdmin={true}/>
+                    <Paginate pages={pages} page={page} isSeller={true}/>
                 </div>
                 )
 
