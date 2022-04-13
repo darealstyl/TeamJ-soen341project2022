9d348e565b7702dc5a4c02ae02fed2583244f63e
z-bruce <87652520+z-bruce@users.noreply.github.com>
2022-04-13 00:35:18 -0400

    Added orders to Profile Screen
    also removed unnecessary link to My Orders in header, orders accessed through My Account

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Form, FormGroup, Button, Row, Col } from "react-bootstrap";
+import { Form, FormGroup, Button, Row, Col, Table } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 import { register } from "../actions/userActions";
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import { LinkContainer } from 'react-router-bootstrap'
 import { getUserDetails, updateUserProfile} from '../actions/userActions'
 import { USER_UPDATE_PROFILE_RESET } from '../constants/userConstants'
+import { listMyOrders } from '../actions/orderActions'
 
 function ProfileScreen({ history }) {
   
@@ -18,7 +20,6 @@ function ProfileScreen({ history }) {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [isSeller, setIsSeller] = useState(false);
 
-  //const [image, setImage] = useState("");
   const [message, setMessage] = useState("");
 
   const userDetails = useSelector((state) => state.userDetails);
@@ -29,8 +30,10 @@ function ProfileScreen({ history }) {
 
   const userUpdateProfile = useSelector((state) => state.userUpdateProfile);
   const { success } = userUpdateProfile;
-  
 
+  const orderListMy = useSelector(state => state.orderListMy)
+  const { loading: loadingOrders, error: errorOrders, orders } = orderListMy
+  
   const dispatch = useDispatch(); 
   let navigate = useNavigate();
   let { slug } = useParams();
@@ -45,6 +48,7 @@ function ProfileScreen({ history }) {
         if(!user || !user.name || success){
             dispatch({type:USER_UPDATE_PROFILE_RESET})
             dispatch(getUserDetails('profile'))
+            dispatch(listMyOrders())
         }
         else{
             setName(user.name)
@@ -166,6 +170,45 @@ function ProfileScreen({ history }) {
         </Col>
         <Col md={9}>
                 <h2>My Orders</h2>
+                {loadingOrders ? (
+                    <Loader />
+                ) : errorOrders ? (
+                    <Message variant='danger'>{errorOrders}</Message>
+                ) : (
+                            <Table striped responsive className='table-sm'>
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Date</th>
+                                        <th>Total</th>
+                                        <th>Paid</th>
+                                        <th>Delivered</th>
+                                        <th>Details</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    {orders.map(order => (
+                                        <tr key={order._id}>
+                                            <td>{order._id}</td>
+                                            <td>{order.createdAt.substring(0, 10)}</td>
+                                            <td>${order.totalPrice}</td>
+                                            <td>{order.isPaid ? order.paidAt.substring(0, 10) : (
+                                                <i className='fas fa-times' style={{ color: 'red' }}></i>
+                                            )}</td>
+                                            <td>{order.isDelivered ? order.deliveredAt.substring(0, 10) : (
+                                                <i className='fas fa-times' style={{ color: 'red' }}></i>
+                                            )}</td>
+                                            <td>
+                                                <LinkContainer to={`/order/${order._id}`}>
+                                                    <Button className='btn-sm'>Details</Button>
+                                                </LinkContainer>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </Table>
+                        )}
         </Col>
     </Row>
   )
